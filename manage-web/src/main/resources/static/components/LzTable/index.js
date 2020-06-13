0a0b8eb5d93ee25f01c64259d762d8a2a9ce52ca
panli1988 <13160080997@163.com>
2020-06-13 22:14:48 +0800

    微调

--- manage-web/src/main/resources/static/components/LzTable/index.js
+++
@@ -1,8 +0,0 @@
-import LzTable from './src/main.vue';
-
-/* istanbul ignore next */
-LzTable.install = function(Vue) {
-    Vue.component(LzTable.name, LzTable);
-};
-
-export default LzTable;
--- manage-web/src/main/resources/static/components/LzTabs/index.js
+++
@@ -1,8 +0,0 @@
-import LzTabs from './src/main.vue';
-
-/* istanbul ignore next */
-LzTabs.install = function(Vue) {
-    Vue.component(LzTabs.name, LzTabs);
-};
-
-export default LzTabs;
